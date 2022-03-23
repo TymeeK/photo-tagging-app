f8db384e8c5fae62c4e060e5aba407d4d095f0e5
Tymee Kong <tymeekong562@gmail.com>
2022-03-23 14:06:20 -0700

    Added mouse tracking for img

--- photo-tagging/src/App.js
+++ photo-tagging/src/App.js
@@ -1,36 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { db } from './firebase-config';
-import { collection, addDoc, getDocs } from 'firebase/firestore';
+import { Flex } from './Styles/App.Style';
+
+//Waldo is between x coordinate 980 and 1010, and y coordinate 355 and 410
 
 function App() {
-    const [user, setUser] = useState([]);
+    const [x, setX] = useState();
+    const [y, setY] = useState();
 
-    useEffect(() => {
-        const writeUser = async () => {
-            const docRef = await addDoc(collection(db, 'users'), {
-                name: 'Tim',
-                age: 23,
-            });
-        };
-
-        const fetchUsers = async () => {
-            const data = await getDocs(collection(db, 'users'));
-            setUser(data.docs.map(doc => ({ ...doc.data() })));
-        };
-        // writeUser();
-        fetchUsers();
-    }, []);
-
+    function handleClick(event) {
+        console.log(event.nativeEvent.offsetX);
+        console.log(event.nativeEvent.offsetY);
+    }
     return (
-        <div className='App'>
-            {user.map(user => {
-                return (
-                    <div>
-                        <h1>Name: {user.name}</h1> <h1>Age: {user.age}</h1>{' '}
-                    </div>
-                );
-            })}
-        </div>
+        <>
+            <Flex>
+                <img
+                    onClick={handleClick}
+                    src={require('./Images/waldo.jpg')}
+                    alt='waldo'
+                />
+                <h1>x: {x}</h1>
+                <h1>y: {y}</h1>
+            </Flex>
+        </>
     );
 }
 
