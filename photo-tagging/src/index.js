5d8f75abc2586d7d07598d39c49a45c3a4405c63
Tymee Kong <tymeekong562@gmail.com>
2022-03-28 12:49:33 -0700

    Connected Scoreboard.js to react router

--- photo-tagging/src/index.js
+++ photo-tagging/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './Styles/index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Scoreboard from './Scoreboard';
 
 ReactDOM.render(
     <React.StrictMode>
         <Router>
             <Routes>
                 <Route path='/' element={<App />} />
+                <Route path='/scoreboard' element={<Scoreboard />} />
             </Routes>
         </Router>
     </React.StrictMode>,
