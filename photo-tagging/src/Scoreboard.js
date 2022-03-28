80a77475f70079d5af247bb93164f621e0ceb858
Tymee Kong <tymeekong562@gmail.com>
2022-03-28 13:21:00 -0700

    Able to get all users and display them onto scoreboard

--- photo-tagging/src/Scoreboard.js
+++ photo-tagging/src/Scoreboard.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getAllUsers } from './firebase-config';
 
 export default function Scoreboard() {
-    return <div></div>;
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const getUsers = async () => {
+            const tempUsers = await getAllUsers();
+            setUsers(tempUsers);
+        };
+        getUsers();
+
+        // setUsers(getAllUsers());
+    }, []);
+
+    useEffect(() => {
+        console.log(users);
+    }, [users]);
+
+    //We want to make a call to firebase to retrieve all users
+    return (
+        <div>
+            {users.map(users => {
+                return (
+                    <div>
+                        <h1>{users.username}</h1>{' '}
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
