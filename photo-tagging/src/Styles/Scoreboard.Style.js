b6d6e239bf90eedc8506ca13cf5893ce1c7bbb5a
Tymee Kong <tymeekong562@gmail.com>
2022-03-30 12:55:00 -0700

    Removed commented code

--- photo-tagging/src/Styles/Scoreboard.Style.js
+++ photo-tagging/src/Styles/Scoreboard.Style.js
@@ -1,15 +1,5 @@
 import styled from 'styled-components';
 
-// export const Grid = styled.div`
-//     display: grid;
-//     grid-template-rows: repeat(${props => props.numUsers}, 100px);
-//     height: 100vh;
-//     width: 1000px;
-//     background-color: brown;
-//     font-weight: bold;
-//     font-size: 25px;
-// `;
-
 export const Flex = styled.div`
     display: flex;
     justify-content: center;
@@ -33,12 +23,3 @@ export const TableDetails = styled.td`
     border-bottom: 1px solid black;
     font-size: 35px;
 `;
-// export const Element = styled.div`
-//     display: flex;
-//     align-items: center;
-//     border-bottom: 1px solid black;
-//     border-left: 1px solid black;
-//     border-right: 1px solid black;
-//     margin-top: 0.5px;
-//     padding-left: 20px;
-// `;
