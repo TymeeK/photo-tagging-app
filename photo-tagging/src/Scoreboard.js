b0624a391ae0eae0166b951227deb8c6cc488823
Tymee Kong <tymeekong562@gmail.com>
2022-03-30 01:12:47 -0700

    Added flexbox so grid can be centered to page.

--- photo-tagging/src/Scoreboard.js
+++ photo-tagging/src/Scoreboard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getAllUsers } from './firebase-config';
-import { Grid } from './Styles/Scoreboard.Style';
+import { Grid, Flex, Element } from './Styles/Scoreboard.Style';
 
 export default function Scoreboard() {
     const [users, setUsers] = useState([]);
@@ -14,15 +14,17 @@ export default function Scoreboard() {
     }, []);
 
     return (
-        <Grid numUsers={users.length}>
-            {users.map((user, index) => {
-                return (
-                    <div key={index}>
-                        <label htmlFor=''>{user.username}</label>
-                        <label htmlFor=''> {user.seconds}</label>
-                    </div>
-                );
-            })}
-        </Grid>
+        <Flex>
+            <Grid numUsers={users.length}>
+                {users.map((user, index) => {
+                    return (
+                        <Element key={index}>
+                            <label>{user.username}</label>
+                            <label> {user.seconds} seconds</label>
+                        </Element>
+                    );
+                })}
+            </Grid>
+        </Flex>
     );
 }
--- photo-tagging/src/Styles/Scoreboard.Style.js
+++ photo-tagging/src/Styles/Scoreboard.Style.js
@@ -2,7 +2,24 @@ import styled from 'styled-components';
 
 export const Grid = styled.div`
     display: grid;
-    grid-template-rows: repeat(${props => props.numUsers}, 25px);
+    grid-template-rows: repeat(${props => props.numUsers}, 100px);
+    height: 100vh;
+    width: 1000px;
+    background-color: brown;
+    font-weight: bold;
+    font-size: 25px;
+`;
+
+export const Flex = styled.div`
+    display: flex;
+    justify-content: center;
     height: 100vh;
     width: 100vw;
+`;
+
+export const Element = styled.div`
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid black;
+    margin-top: 0.5px;
 `;
